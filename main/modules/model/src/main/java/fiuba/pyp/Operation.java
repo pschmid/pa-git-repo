0f8e9ac93b82eb77588d37cbd5f7c9b401b3b3e8
Paulo Dellanzo <paulodellanzo@gmail.com>
2014-01-19 20:05:35 -0300

    Agrego Clases Basicas

---
+++ main/modules/model/src/main/java/fiuba/pyp/Operation.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package fiuba.pyp;
+
+/**
+ * @author pyp
+ *
+ */
+public class Operation {
+	
+	private String Document;
+	
+	public void Insert(String s){
+		Document.concat(s);
+	}
+	
+	public void Delete(String s){
+		Document.length();
+	}
+	
+}
---
+++ main/modules/model/src/main/java/fiuba/pyp/Transformation.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package fiuba.pyp;
+
+/**
+ * @author pyp
+ *
+ */
+public abstract class Transformation {
+	
+	public Operation transform(Operation op1, Operation op2){
+		return op1;
+	}
+
+}
