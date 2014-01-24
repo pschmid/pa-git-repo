91d942d53dcbab5681506ca5c809d34ee01e4ead
Pablo Schmid <schmidpablo@gmail.com>
2014-01-24 20:55:54 -0300

    adding log4j

--- main/modules/engine/goto/src/main/java/fiuba/pyp/App.java
+++ main/modules/engine/goto/src/main/java/fiuba/pyp/App.java
@@ -1,13 +1,26 @@
 package fiuba.pyp;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+
     public static void main( String[] args )
     {
+        PropertyConfigurator.configure(App.class.getClassLoader().getResource(
+                "log4j.properties"));
+        Logger log = Logger.getLogger(App.class);
         System.out.println( "Hello World! Pyp" );
+        // Configure Log4J
+
+
+        log.info( "Hello World! Pyp" );
+
     }
 }
