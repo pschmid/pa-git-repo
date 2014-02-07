8b05c8a704819f6d4a40d0c00612cdb70ad84fe5
Paulo Dellanzo <paulodellanzo@gmail.com>
2014-02-07 15:32:55 -0300

    Quito logger todavia no me corre en Eclipse

--- main/modules/engine/goto/src/main/java/fiuba/pyp/App.java
+++ main/modules/engine/goto/src/main/java/fiuba/pyp/App.java
@@ -13,30 +13,35 @@ public class App
 
     public static void main( String[] args )
     {
-        PropertyConfigurator.configure(App.class.getClassLoader().getResource(
-                "log4j.properties"));
-        Logger log = Logger.getLogger(App.class);
+//        PropertyConfigurator.configure(App.class.getClassLoader().getResource(
+//                "log4j.properties"));
+//        Logger log = Logger.getLogger(App.class);
         System.out.println("Hello World! Pyp");
         // Configure Log4J
         ConcurrencyControl concurrencyControl = ConcurrencyControl.getInstance();
         concurrencyControl.setDoc(new DocumentText());
 
         Operation a = new Operation(new DocumentCharacter("a"), 0, "INSERT", 1, 1);
-        Operation b = new Operation(new DocumentCharacter("b"), 0, "INSERT", 2, 2);
+        Operation b = new Operation(new DocumentCharacter("b"), 1, "INSERT", 2, 2);
         b.setOtherSitesOperations(a);
         Operation c = new Operation(new DocumentCharacter("c"), 0, "INSERT", 3, 1);
 
-        Operation d = new Operation(new DocumentCharacter("d"), 0, "INSERT", 1, 3);
+        Operation d = new Operation(new DocumentCharacter("d"), 2, "INSERT", 1, 2);
         Operation e = new Operation(new DocumentCharacter("e"), 0, "INSERT", 1, 3);
 
-        Operation f = new Operation(new DocumentCharacter("f"), 0, "INSERT", 1, 3);
+        Operation f = new Operation(new DocumentCharacter("f"), 3, "INSERT", 1, 4);
 
 
         concurrencyControl.run(a);
         concurrencyControl.run(b);
-        //concurrencyControl.run(c);
+//        concurrencyControl.run(c);
+        concurrencyControl.run(d);
+        concurrencyControl.run(e);
 
-        log.info( "Hello World! Pyp" );
-        log.info(concurrencyControl.getDoc().toString());
+//        log.info( "Hello World! Pyp" );
+//        log.info(concurrencyControl.getDoc().toString());
+        System.out.println("\n");
+        System.out.println(concurrencyControl.getDoc().toString());
+        
     }
 }
